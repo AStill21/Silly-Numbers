bdef626744e4704f21f5b104ba3c37cc99addc3c
Amiya Stillwell <stillwell.amiya@gmail.com>
2022-02-15 19:04:44 -0800

    created and started README document with test 1 -2 complete.

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,8 @@ const numberArray["0""1""2"3,4,5,6,7,8,9,10]
 const inputtedNumber= parseInt(inputtedNumber);
 const inputted1 = "boop";
 const inputted2 = "beep";
-const inpuuted3 = "beep";
+const inpuuted3 = "Won't you be my neighbor?";
+
 
 forEach(inputtedNumber
 
