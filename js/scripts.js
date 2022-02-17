726766ed4797c30c9598f61ae5911cab186b42f9
Amiya Stillwell <stillwell.amiya@gmail.com>
2022-02-16 20:30:37 -0800

    creaded if...else statment for phrases: boop, beep, 'won't you be my neighbor?' and associated them with the numbers: 1,2,3

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,5 @@
-const sillyArray["boop" "beep" "Won't you be my neighbor?"];
-
-function numberIncrease(numberInput) {
-  const numberInput = parseInt();
-  const result= numberInput(number1)
-}
-
-
-
-// const numberArray["0""1""2"3,4,5,6,7,8,9,10]
-// const inputtedNumbers= parseInt(inputtedNumber);
-// const inputted1 = "boop";
-// const inputted2 = "beep";
-// const inpuuted3 = "Won't you be my neighbor?";
-// forEach(inputtedNumber
-// for(index=1; index<=300; index+=1)
+const array = [];
+const newArray = array.map(function(element) {
+  return element+1;
+});
+newArray;
