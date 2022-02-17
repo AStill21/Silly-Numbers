89f88ce0c5874b2f2a6ec98ced421be48ce8779f
Amiya Stillwell <stillwell.amiya@gmail.com>
2022-02-16 20:55:37 -0800

    updates for statement in business logicfor index=0

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,21 @@
+// Business Logic
 const array = [];
 const newArray = array.map(function(element) {
-  return element+1;
+for(let index=0; index<=inputtedNumber index +=1);
 });
-newArray;
+
+if (inputtedNumber.includes('3')) {
+  return newArray.push("Won't you be my neighbor?");
+} else if (inputtedNumber.includes('2')) {
+  return newArray.push('Beep!');
+} else (inputtedNumber.includes('1')) {
+  return newArray.push('Boop!')
+};
+
+
+
+// UI logic
+
+$(document).ready(function() {
+  
+});
